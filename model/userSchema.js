726d35611c53686a0756711e910701c603f868f7
Rishu Singh <rishu04072001@gmail.com>
2021-09-09 16:30:40 +0530

    make it better

--- model/userSchema.js
+++ model/userSchema.js
@@ -30,7 +30,6 @@ const userSchema = new mongoose.Schema({
     favourites:[String]
 })
 
-
 userSchema.pre('save',async function(next){
     if(this.isModified('password')){
         this.password = await bcrypt.hash(this.password,12)
